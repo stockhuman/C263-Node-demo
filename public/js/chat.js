1b6b34284899b94ac5a39bcf871963db78c0c313
Michael Hemingway <mpghemingway@gmail.com>
2019-02-25 18:54:44 -0500

    Stylistic improvements to code and presentation :alien:

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const socket = io()
 
 	function scrollToBottom() {
-		const messages = jQuery('#messages')
+		const messages = $('#messages')
 		const newMessage = messages.children('li:last-child')
 		const clientHeight = messages.prop('clientHeight')
 		const scrollTop = messages.prop('scrollTop')
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	socket.on('connect', function () {
-		const params = jQuery.deparam(window.location.search)
+		const params = $.deparam(window.location.search)
 
 		socket.emit('join', params, function (err) {
 			if (err) {
@@ -32,50 +32,50 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	socket.on('newMessage', function (message) {
-		const formattedTime = moment(message.createdAt).format('h:mm:a')
-		const template = jQuery('#message-template').html()
+		const formattedTime = moment(message.createdAt).format('h:mm A')
+		const template = $('#message-template').html()
 		const html = Mustache.render(template, {
 			text: message.text,
 			from: message.from,
 			createdAt: formattedTime
 		})
 
-		jQuery('#messages').append(html)
+		$('#messages').append(html)
 
 		scrollToBottom()
 	})
 
 	socket.on('newNotice', function (message) {
 		const formattedTime = moment(message.createdAt).format('h:mm:a')
-		const template = jQuery('#notice-template').html()
+		const template = $('#notice-template').html()
 		const html = Mustache.render(template, {
 			text: message.text,
 			createdAt: formattedTime
 		})
 
-		jQuery('#messages').append(html)
+		$('#messages').append(html)
 	})
 
 	socket.on('updateUserList', function (users) {
-		const ol = jQuery('<ol></ol>')
+		const ol = $('<ol></ol>')
 		users.forEach(function (user) {
-			ol.append(jQuery('<li></li>').text(user))
+			ol.append($('<li></li>').text(user))
 		})
 
-		jQuery('#users').html(ol)
+		$('#users').html(ol)
 	})
 
 	socket.on('newLocationMessage', function (message) {
 
 		const formattedTime = moment(message.createdAt).format('h:mm:a')
-		const template = jQuery('#location-message-template').html()
+		const template = $('#location-message-template').html()
 		const html = Mustache.render(template, {
 			text: message.url,
 			from: message.from,
 			createdAt: formattedTime
 		})
 
-		jQuery('#messages').append(html)
+		$('#messages').append(html)
 
 		scrollToBottom()
 	})
@@ -84,19 +84,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.log('Disconnected from Server')
 	})
 
-	jQuery('#message-form').on('submit', function (e) {
+	$('#message-form').on('submit', function (e) {
 		e.preventDefault()
 
-		const messageTextbox = jQuery('[name=message]')
+		const messageTextbox = $('[name=message]')
 
 		socket.emit('createMessage', {
-			text: jQuery('[name=message]').val()
+			text: $('[name=message]').val()
 		}, function () {
 			messageTextbox.val('')
 		})
 	})
 
-	const locationButton = jQuery('#send-location')
+	const locationButton = $('#send-location')
 	locationButton.on('click', function () {
 
 		if (!navigator.geolocation) {
